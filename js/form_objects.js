68fe6c689c1a076646fd4202ad8468499344691b
RAmi Glez <rdelacalle30@gmail.com>
2016-05-31 10:57:56 -0400

    uploading first files

---
+++ js/form_objects.js
@@ -0,0 +1,7 @@
+ var forms = Array();
+ (function () {
+     for (i = 0; i < 10; i++) {
+         forms.push({ formname: 'form' + i.tostring(), properties: [{ name: 'FIELD1', type: 'Text' }, { name: 'FIELD2', type: 'Number' }] });
+     }
+ });
+
