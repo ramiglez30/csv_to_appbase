34f9e544c7cbe0f41f3f81e6e92ffe89c6e9e76e
Rami Glez <rdelacalle30@gmail.com>
2016-06-09 13:29:39 -0400

    fixed file changing. first column as delete button.

---
+++ async.js
@@ -0,0 +1,34 @@
+Function.prototype.applyAsync = function(params, cb){
+  var function_context = this;
+  setTimeout(function(){
+      var val = function_context.apply(this, params);
+      if(cb) cb(val);
+  }, 0);
+};
+
+var loggingSync = function(){
+  console.log('iniciando');
+};
+
+var message ="func 2 1st hit"
+
+var Func2 = function(mess) {
+  console.log(mess);
+}
+
+var Func3 = function() {
+  console.log('func 3');
+}
+
+var Func4 = function() {
+  console.log('func 4');
+}
+var Func5 = function() {
+  console.log('func 5');
+}
+
+loggingSync();
+Func2.applyAsync([message], ()=>{console.log('finished')});
+Func3();
+Func4();
+Func5();
