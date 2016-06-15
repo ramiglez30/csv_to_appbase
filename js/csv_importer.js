ca892ff124b88ef0008fbe3399e0638938df0d57
RAmi Glez <rdelacalle30@gmail.com>
2016-06-15 10:37:01 -0400

    listo para conectar

---
+++ js/csv_importer.js
@@ -0,0 +1,131 @@
+$.extend({
+    json_array: []
+});
+
+$(document).ready(function() {
+    //datos parseados a JSON
+    var formObject = null;
+    $.json_array = [];
+    //function parser del texto CSV a un objeto JSON
+    var parseCSVtoJSON = function(content) {
+       $.json_array = Papa.parse(content, {
+            dynamicTyping: true,
+            header: false
+        });
+   };
+
+   //llena la tabla del mapping
+    var seedTableData = function(){
+        $("table#table_mapping tbody").children().remove();
+        $("table#table_mapping thead").children().remove();
+        $('select#form_names option:selected').removeAttr('selected');
+
+        if($.json_array.length == 0) {
+            parseCSVtoJSON();
+        }
+
+        var counter = rowCount();
+        //if(rowCount() < 10) {counter = rowCount();}
+
+        var a = 0;
+
+        for(i = a; i < counter; i++) {
+            var rowData = $('<tr></tr>');
+            rowData.attr('data-index', i.toString());
+            var del_button = $('<td><a style="cursor:pointer" data-toggle="tooltip" data-placement="left" data-index="'+i.toString()+'" title="Delete this Row"><span class="glyphicon glyphicon-remove text-danger"><span></a></td>');
+            del_button.children('a').on('click', function(e){
+                                                        let data_index = $(this).attr('data-index');
+                                                        let row_index = $('table#table_mapping tr[data-index="'+data_index.toString()+'"]').index();
+                                                        $('table#table_mapping tr[data-index="'+data_index.toString()+'"]').remove();
+                                                        $.json_array.data.splice(row_index, 1);
+                                                    });
+            del_button.appendTo(rowData);
+            for (var k = 0; k < columnCount(); k++){
+                $('<td>'+$.json_array.data[i][k]+'</td>').appendTo(rowData);
+            }
+            $("table#table_mapping tbody").append(rowData);
+        }
+   };
+
+    //event handler para el select form
+    $('select#form_names').on('change',function() {
+         $.appBaseService.getForms((result) => { formObject = result[this.options[this.selectedIndex].value]} );
+         seedHeadingMaps();
+     });
+
+
+    //event handler para cuando el usuario edita el texto
+    $("#file_content").on("input", function(event) {
+        parseCSVtoJSON.applyAsync([], seedTableData);
+    });
+
+
+
+   var seedHeadingMaps = function() {
+        if(formObject == null){return;}
+        var columnProperties = formObject.properties;
+        //clean datatable
+        $("table#table_mapping thead").children().remove();
+           var row = $('<tr></tr>');
+           var emptycell = $('<th></th>');
+           emptycell.appendTo(row);
+        for(var j = 0; j<columnCount(); j++){
+            var headingCell = $('<th></th>');
+            var select = $('<select></select>', {id: "select_"+j.toString(), 'class':'form-control'});
+            var ignore = $('<option>ignore</option>', {'data-value': 'ignore'});
+            ignore.appendTo(select);
+            for(var k = 0; k< columnProperties.length; k++){
+                var option = $('<option>'+columnProperties[k].name+'</option>');
+                option.attr('data-name', columnProperties[k].name);
+                option.attr('data-order', columnProperties[k].order);
+                option.attr('data-type', columnProperties[k].type);
+                option.attr('data-isReference', columnProperties[k].isReference);
+                if(columnProperties[k].isReference=="true"){
+                    option.attr('data-formReferenced', columnProperties[k].formReferenced);
+                    option.attr('data-dataReferenced', columnProperties[k].dataReferenced);
+                }
+                option.appendTo(select);
+            }
+            select.appendTo(headingCell);
+            headingCell.appendTo(row);
+        }
+        $('table#table_mapping thead').append(row);
+   };
+
+   var columnCount = function() {
+     if ($.json_array != null){
+        return $.json_array.data[1].length;
+     } else { return 0;}
+   };
+    var rowCount = function() {
+     if ($.json_array != null){
+        return $.json_array.data.length;
+     } else { return 0;}
+   };
+
+    //carga fichero y contenido en el textarea
+    $('#csv_file_input').bind('change', function(event) {
+        var input = event.target;
+
+        var reader = new FileReader();
+        reader.onload = function() {
+            var text = reader.result.split(' ').filter(n => n).join(' ');
+            var content = document.getElementById('file_content');
+            content.value = text;
+            parseCSVtoJSON.applyAsync([text], seedTableData);
+        }
+        reader.readAsText(input.files[0]);
+
+    });
+    //busca los datos desde el appbase
+    $.appBaseService.getForms(function(result) {
+        var keys = Object.keys(result);
+        keys.forEach(key => {
+            var drop_forms = document.getElementById('form_names');
+            var option = document.createElement('option');
+            option.text = key;
+            drop_forms.add(option);
+        });
+    });
+
+});
